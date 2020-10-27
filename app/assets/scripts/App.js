de82d72e6e808ed48328968c778c16cfcc25fc13
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-27 11:59:10 +0100

    removing jquery & scripts to scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,8 @@
 import '../styles/styles.css';
-import $ from 'jquery';
 import Functions from './modules/Function';
 
-var functions = new Functions();
 
 
 
 
-
-if (module.hot) {
-  module.hot.accept();
-}
+if (module.hot) { module.hot.accept(); }
